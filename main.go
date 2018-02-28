1670a87b25d3bf0541dbd1e7d9385802fd946e09
Jonathan Pigrée <jonathan.pigree@gmail.com>
2018-02-28 17:31:29 +0100

    #3 Initialize viper and configuration file

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"github.com/kube-ops/pops/cmd"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	InitializeConfig()
+
+	if err := ReadConfigFile(); err != nil {
+		log.Warn(err)
+	}
+
 	cmd.Execute()
 }
