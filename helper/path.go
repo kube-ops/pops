352f643fc3541dd4811e72fe9ad2f15500c190cf
Jonathan Pigrée <jonathan.pigree@gmail.com>
2018-03-07 18:27:14 +0100

    #1 Makes the project root dir a config key
    - Fixed bug when the git root is not the current directory.
    - Set the project root dir to the git root if exists, else set it to
    current working directory.

--- helper/path.go
+++ helper/path.go
@@ -1,7 +1,10 @@
 package helper
 
 import (
+	"fmt"
 	"os"
+	"path"
+	"path/filepath"
 )
 
 // Exists returns whether the given file or directory exists or not.
@@ -14,4 +17,28 @@ func Exists(path string) (bool, error) {
 		return false, nil
 	}
 	return true, err
+}
+
+// GitRootDir returns the Git root directory of the current project.
+func GitRootDir() (string, error) {
+	currDir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	for path.Clean(currDir) != "/" {
+		isGitRootDir, fileStatErr := Exists(path.Join(currDir, ".git"))
+
+		if fileStatErr != nil {
+			return "", fileStatErr
+		}
+
+		if isGitRootDir {
+			return currDir, nil
+		}
+
+		currDir = filepath.Dir(currDir)
+	}
+
+	return "", fmt.Errorf("No Git root accessible from the current working directory")
 }
